414c0a96a1cc5a86a9d49bf9a66dc8bc172448d3
hdo <dhanshans@yahoo.com>
2019-03-28 14:33:04 +0100

    nouvelle classe ClassAHans

---
+++ src/main/java/pro/ClassAHans.java
@@ -0,0 +1,23 @@
+package pro;
+
+public class ClassAHans {
+
+    private String name = "Hans Dooh";
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassAHans{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
