3ccdcccc433a837b649ab34d5f355a965e7c581f
uince <umit.ince@free.fr>
2019-03-28 14:50:23 +0100

    add param properties equipe

--- src/main/java/pro/App.java
+++ src/main/java/pro/App.java
@@ -1,5 +1,7 @@
 package pro;
 
+import java.util.ResourceBundle;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Salut les amis!" );
+
+
+        String equipeNumber = ResourceBundle.getBundle("param").getString("equipe.number");
+        System.out.println("Equipe Number = " + equipeNumber);
     }
 }
